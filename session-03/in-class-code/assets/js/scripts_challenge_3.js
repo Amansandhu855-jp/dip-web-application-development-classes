7332e3380fd19d6e9ed0cd9c2c4f89b8fc466e94
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2026-02-17 15:19:55 +0800

    feat: class object

--- session-03/in-class-code/assets/js/scripts_challenge_3.js
+++ session-03/in-class-code/assets/js/scripts_challenge_3.js
@@ -1,8 +1,51 @@
 // Challenge 3 Starter
 // Create the class and use the constructor method
-class UserObject {}
+class UserObject {
+  constructor(
+    firstName,
+    lastName,
+    age,
+    isStudent,
+    address,
+    skills,
+    socialProfiles,
+    isEmployed,
+    changeAge,
+    changeArrowAge,
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.isStudent = isStudent;
+    this.address = address;
+    this.skills = skills;
+    this.socialProfiles = socialProfiles;
+    this.isEmployed = isEmployed;
+    this.changeAge = changeAge;
+    this.changeArrowAge = changeArrowAge;
+  }
+}
 
 // Create an instance of the ComplexObject class (hint: add the properties)
-const userObjectInstance = {};
+const user1 = new UserObject(
+  "Aldus",
+  "Huxley",
+  30,
+  false,
+  "123 Main St Perth 6000",
+  ["JavaScript", "HTML", "CSS"],
+  {
+    twitter: "@aldus",
+    linkedIn: "linkedin.com/in/ahuxley",
+  },
+  true,
+  function () {
+    return (this.age += 1);
+  },
+  () => {
+    this.age += 1;
+  },
+);
 
 // Log the new instance of the object
+console.log(user1);
---
+++ session-03/in-class-code/assets/js/userobjects.js
@@ -0,0 +1,71 @@
+const users = [
+  {
+    firstName: "Joghn",
+    lastName: "Huxley",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@aldus",
+      linkedIn: "linkedin.com/in/ahuxley",
+    },
+    isEmployed: true,
+    changeAge: function () {
+      return (this.age += 1);
+    },
+    changeArrowAge: () => {
+      this.age += 1;
+    },
+  },
+  {
+    firstName: "Vu",
+    lastName: "Huxley",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@aldus",
+      linkedIn: "linkedin.com/in/ahuxley",
+    },
+    isEmployed: true,
+    changeAge: function () {
+      return (this.age += 1);
+    },
+    changeArrowAge: () => {
+      this.age += 1;
+    },
+  },
+  {
+    firstName: "Gun",
+    lastName: "Huxley",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@aldus",
+      linkedIn: "linkedin.com/in/ahuxley",
+    },
+    isEmployed: true,
+    changeAge: function () {
+      return (this.age += 1);
+    },
+    changeArrowAge: () => {
+      this.age += 1;
+    },
+  },
+];
