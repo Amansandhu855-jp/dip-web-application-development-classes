a3492df9ee0e309ab0f1edf90600372e8ec95e97
Amansandhu855-jp <samratsandhu855@gmail.com>
2026-04-30 14:27:46 +0800

    Implement logic to find and log numbers up to 5

--- session-02/assets/js_starter/scripts_challenge_4.js
+++ session-02/assets/js_starter/scripts_challenge_4.js
@@ -1,13 +1,20 @@
 // Challenge 4 Starter
+(function () {
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5, 6];
 
 // Find the index of the number 5 in the array
-
+let index = numbers.findIndex(num => num === 5);
 
 // If the number 5 is found in the array
-
+if (index !== -1) {
+  // print each number up to and including 5
+  numbers.slice(0, index + 1).forEach(num => { 
   // Console log each number in the array up to and including 5
-
+ console.log(num);
+     });
+  } else {
   // Output a message if the number 5 is not in the array
   console.log("Number not in array")
+}
+  })(); 
