3f439b4b63e730bc6a49784d1dd7425803d15108
Amansandhu855-jp <samratsandhu855@gmail.com>
2026-04-30 11:10:30 +0800

    Define arrow function to double array numbers

--- session-01/assets/js_starter/scripts_challenge_2.js
+++ session-01/assets/js_starter/scripts_challenge_2.js
@@ -4,8 +4,9 @@ let numbers = [1, 2, 3, 4, 5];
 
 // Define the arrow function doubleNumbers
 
-
+const doubleNumbers = (arr) => arr.map(num => num * 2);
 // Call the function with the numbers array
+let result = doubleNumbers(numbers);
 
 
 // Console log the new array
